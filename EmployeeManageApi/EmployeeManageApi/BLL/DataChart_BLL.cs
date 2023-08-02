22e5cec5e4699defaf9f9e74a760043629e8557e
1944548530 <1944548530@qq.com>
2023-08-02 20:20:44 +0800

    加班提报修改

---
+++ EmployeeManageApi/EmployeeManageApi/BLL/DataChart_BLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManageApi.BLL
+{
+    public class DataChart_BLL
+    {
+        //public string GetDeptAttendance() { 
+            
+        //}
+    }
+}
---
+++ EmployeeManageApi/EmployeeManageApi/Controllers/DataChartController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManageApi.BLL;
+using EmployeeManageApi.Common;
+using EmployeeManageApi.ResponseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManageApi.Controllers
+{
+    public class DataChartController : Controller
+    {
+        DataChart_BLL bll = new DataChart_BLL();
+        // GET: DataChart
+        //public JsonResult GetDeptAttendance()
+        //{
+        //    var response = new Response();
+        //    try
+        //    {
+
+        //    }
+        //    catch (Exception e) {
+        //        response.SetFailed("服务器内部错误!");
+        //        LoggerHelper.WriteLog(e);
+        //        return Json(response, JsonRequestBehavior.AllowGet);
+        //    }
+        //}
+    }
+}
